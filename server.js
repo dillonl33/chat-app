469236ba7dac8a059094f4a0c514904b1f05abeb
dillonl33 <90289353+dillonl33@users.noreply.github.com>
2021-12-03 22:48:46 -0600

    we pogging

--- server.js
+++ server.js
@@ -14,6 +14,80 @@ const {
 const loginStuff = require('./public/js/login');
 
 const app = express();
+
+// START CODE FROM LOGIN
+
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+app.get('/', function(request, response) {
+	response.sendFile(path.join(__dirname + '/login.html'));
+});
+
+app.post('/auth2', function(request, response) {
+	console.log('can we see this?');
+	var username = request.body.username;
+	var password = request.body.password;
+	if (username && password) {
+		client.query('SELECT * FROM user_passwords WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.username = username;
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+app.get('/auth2', function(request, response) {
+	console.log('can we see this?');
+	var username = request.body.username;
+	var password = request.body.password;
+	if (username && password) {
+		client.query('SELECT * FROM user_passwords WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.username = username;
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+app.get('/home', function(request, response) {
+	if (request.session.loggedin) {
+		response.send('Welcome back, ' + request.session.username + '!');
+	} else {
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});
+
+// END CODE FROM LOGIN
+
+
+
+
+
+
+
 const server = http.createServer(app);
 const io = socketio(server);
 
