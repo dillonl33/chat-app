4e4853414fac43be57b7aebf016b8074a896bb40
daniellemoreno525 <daniellemoreno525@tamu.edu>
2021-12-04 17:03:12 -0600

    update href to be the user logged in

--- public/js/home.js
+++ public/js/home.js
@@ -7,5 +7,10 @@ socket.on('theName', (theName) => {
 
     document.getElementById('username').innerHTML = "Username: "+theName;
 
-    // document.getElementById('personalChat1').href = "";
+    document.getElementById('personalChat1').href = "../chat.html?username="+theName+"&room=1";
+    document.getElementById('personalChat2').href = "../chat.html?username="+theName+"&room=2";
+    document.getElementById('personalChat3').href = "../chat.html?username="+theName+"&room=3";
+    document.getElementById('globalChat1').href = "../chat.html?username="+theName+"&room=4";
+    document.getElementById('globalChat2').href = "../chat.html?username="+theName+"&room=5";
+    document.getElementById('globalChat3').href = "../chat.html?username="+theName+"&room=6";
 });
