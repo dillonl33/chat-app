0ad413a74330fd3750ef46973140419b22c1ba12
daniellemoreno525 <daniellemoreno525@tamu.edu>
2021-12-04 14:38:42 -0600

    attempting to display username on home page

--- public/js/home.js
+++ public/js/home.js
@@ -2,7 +2,7 @@ const socket = io();
 
 socket.emit('getName');
 
-socket.on('username', (theName) => {
+socket.on('theName', (theName) => {
     console.log('we got the username as: ' + theName);
 
     document.getElementById('username').innerHTML = "Username: "+theName;
