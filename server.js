d9be02c45cc916e0f997d411f9e386cd6f5c2ba3
Haoming Yuan <depasinre@gmail.com>
2021-12-05 11:39:47 -0600

    v2.2

--- server.js
+++ server.js
@@ -312,7 +312,7 @@ io.on('connection', socket => {
     let regist_query = "INSERT INTO user_passwords (username, password) VALUES ('" + username + "', '" + password +"');";
     let profile_create_query = "INSERT INTO users (username, email, firstname, lastname, phone, location, game, day, month, year, discord) VALUES ('" + username + "', 'temp', 'temp', 'temp', 'temp', 'temp', 'temp', 'temp', 'temp', 'temp', 'temp');";
     
-    client.query(profile_create_query);
+    //client.query(profile_create_query);
     client.query(regist_query, (err) => {
       if (err) throw err;
       res.redirect('/');
@@ -326,7 +326,7 @@ io.on('connection', socket => {
     var password = req.body.password;
     let regist_query = "INSERT INTO user_passwords (username, password) VALUES ('" + username + "', '" + password +"');";
     let profile_create_query = "INSERT INTO users (username, email, firstname, lastname, phone, location, game, day, month, year, discord) VALUES ('" + username + "', 'temp', 'temp', 'temp', 'temp', 'temp', 'temp', 'temp', 'temp', 'temp', 'temp');";
-    client.query(profile_create_query);
+    //client.query(profile_create_query);
     client.query(regist_query, (err) => {
       if (err) throw err;
       res.redirect('/');
