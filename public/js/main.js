542fb069716d7e988f5139098afff55d5370a59d
dillonl33 <90289353+dillonl33@users.noreply.github.com>
2021-12-05 00:18:48 -0600

    removed usrs from this (you don't need to see yourself)

--- public/js/main.js
+++ public/js/main.js
@@ -10,7 +10,7 @@
 const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
 const roomName = document.getElementById('room-name');
-const userList = document.getElementById('users');
+//const userList = document.getElementById('users');
 
 
 
@@ -66,7 +66,7 @@ getTheName(function(username, room) {
   // Get room and users
   socket.on('roomUsers', ({ room, users }) => {
     outputRoomName(room);
-    outputUsers(users);
+    /*outputUsers(users);*/
   });
 
   // Message from server
@@ -120,7 +120,7 @@ getTheName(function(username, room) {
     roomName.innerText = room;
   }
   
-  // Add users to DOM
+  /*// Add users to DOM
   function outputUsers(users) {
     userList.innerHTML = '';
     users.forEach((user) => {
@@ -128,7 +128,7 @@ getTheName(function(username, room) {
       li.innerText = user.username;
       userList.appendChild(li);
     });
-  }
+  }*/
   
   //Prompt the user before leave chat room
   document.getElementById('leave-btn').addEventListener('click', () => {
