f6ea2a12a59eb08ad3366f49ad02618c97a2a750
daniellemoreno525 <daniellemoreno525@tamu.edu>
2021-12-05 17:44:50 -0600

    fixing length

--- public/js/recommend.js
+++ public/js/recommend.js
@@ -1,9 +1,9 @@
 const socket = io();
 
 socket.emit('getRecs');
-socket.on('retRecs', (recUsers) => {
+socket.on('retRecs', (recUsers,len) => {
     // console.log("recUser: " + recUser);
-    for (let i = 0; i < recUsers.length(); i++) {
+    for (let i = 0; i < len; i++) {
         document.getElementById('user'+i).innerHTML = recUsers[i].username;
     }
 });
