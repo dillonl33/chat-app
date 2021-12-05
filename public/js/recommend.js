d96d40c094ac008e38f59bb3f28c46c7fdabf19d
daniellemoreno525 <daniellemoreno525@tamu.edu>
2021-12-05 17:52:39 -0600

    removing len and hardcoding a number

--- public/js/recommend.js
+++ public/js/recommend.js
@@ -3,7 +3,7 @@ const socket = io();
 socket.emit('getRecs');
 socket.on('retRecs', (recUsers,len) => {
     // console.log("recUser: " + recUser);
-    for (let i = 0; i < len; i++) {
+    for (let i = 0; i < 3; i++) {
         document.getElementById('user'+i).innerHTML = recUsers[i].username;
     }
 });
