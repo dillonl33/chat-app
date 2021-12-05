af2a522324d5be9259509fc87b7ad0ce910b1dc0
dillonl33 <90289353+dillonl33@users.noreply.github.com>
2021-12-05 01:19:06 -0600

    trying to add chat history pushing

--- server.js
+++ server.js
@@ -229,7 +229,7 @@ app.get('/update', function(req, res) {
   socket.on('chatMessage', msg => {
     const user = getCurrentUser(socket.id);
     // chatid, senderid, receiverid, message, time
-    client.query('INSERT INTO chats values (default, (select uid from users where username = \'' + user.username + '\'), (select uid from users where username = \'' + user.room + '\'), ' + msg + ', CURRENT_TIMESTAMP);', (err, ret) => {
+    client.query('INSERT INTO chats values (default, (select uid from users where username = \'' + user.username + '\'), (select uid from users where username = \'' + user.room + '\'), \'' + msg + '\', CURRENT_TIMESTAMP);', (err, ret) => {
       //client.query('SELECT senderid, message, time FROM chats WHERE senderid = (select uid from users where username = \'' + username + '\') AND receiverid = (select uid from users where username = \'' + room + '\'));', (err, ret) => {
         if (err) throw err;
         /*for (let row of ret.rows) {
