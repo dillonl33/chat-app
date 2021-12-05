d716f5406e239e4c6d480da59c071c9e249903de
daniellemoreno525 <daniellemoreno525@tamu.edu>
2021-12-05 15:39:31 -0600

    Revert "changing from recommend.js to home.js"
    This reverts commit 8d7f8531fe5dbc90148820d6bc3db35dafbfa102.

--- public/js/home.js
+++ public/js/home.js
@@ -62,9 +62,3 @@ socket.on('theGame', (theGame) => {
     document.getElementById('mainGame').innerHTML = "Main Game: "+theGame;
 
 });
-
-socket.emit('getRecs',0);
-
-socket.on('retRecs', (recUser) => {
-    document.getElementById('user0').innerHTML = recUser;
-});
