1061f80359cf8aae6c871f6871066e91a55a77bf
dillonl33 <90289353+dillonl33@users.noreply.github.com>
2021-12-05 01:48:26 -0600

    BRUHHHHHHHHHHHHHHHHHHHHHH

--- server.js
+++ server.js
@@ -194,7 +194,7 @@ app.get('/update', function(req, res) {
       var temp;
       var temp2;
       var temp3 = [];
-      client.query('SELECT senderid, message, time from chats where senderid = (select uid from users where username = \'' + user.username + '\') and receiverid = (select uid from users where username = \'' + user.room + '\');', (err, ret) => {
+      client.query('SELECT senderid, message, time from chats where senderid = (select uid from users where username = \'' + user.room.substring(0,user.room.indexOf('_')) + '\') and receiverid = (select uid from users where username = \'' + user.room.substring(0,user.room.indexOf('_')) + '\');', (err, ret) => {
       //client.query('SELECT senderid, message, time FROM chats WHERE senderid = (select uid from users where username = \'' + username + '\') AND receiverid = (select uid from users where username = \'' + room + '\'));', (err, ret) => {
         if (err) throw err;
         for (let row of ret.rows) {
