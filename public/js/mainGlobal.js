98b80a251fd245e3f57bdd5d0a4016e8a60439c9
dillonl33 <90289353+dillonl33@users.noreply.github.com>
2021-12-05 20:47:35 -0600

    bruh

--- public/js/mainGlobal.js
+++ public/js/mainGlobal.js
@@ -6,7 +6,7 @@
 
 
 const chatForm = document.getElementById('chat-form');
-const chatMessages = document.querySelector('.messages');
+const chatMessages = document.querySelector('.chat-messages');
 const roomName = document.getElementById('room-name');
 const userList = document.getElementById('users');
 
