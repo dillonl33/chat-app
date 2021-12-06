dbca83bba29dcad713d5c043cd33037f0aa3cf03
daniellemoreno525 <daniellemoreno525@tamu.edu>
2021-12-05 21:53:36 -0600

    updating submit form

--- public/js/apexAPI.js
+++ public/js/apexAPI.js
@@ -1,15 +1,13 @@
-function getRank(username) {
-    const h = document.getElementById('rank');
-    const url = 'https://api.mozambiquehe.re/bridge?version=5&platform=PC&player='+username+'&auth='+process.env.APEX_AUTH;
+const h = document.getElementById('rank');
+const url = 'https://api.mozambiquehe.re/bridge?version=5&platform=PC&player='+username+'&auth='+process.env.APEX_AUTH;
 
-    fetch(url)
-    .then((resp) => resp.json())
-    .then(function(data) {
-        let userInfo = data.results;
-        console.log(userInfo.global.rank.rankScore);
-        h.innerHTML = 'Rank: '+userInfo.global.rank.rankScore;
-    })
-    .catch(function(error) {
-        console.log(error);
-    });
-}
+fetch(url)
+.then((resp) => resp.json())
+.then(function(data) {
+    let userInfo = data.results;
+    console.log(userInfo.global.rank.rankScore);
+    h.innerHTML = 'Rank: '+userInfo.global.rank.rankScore;
+})
+.catch(function(error) {
+    console.log(error);
+});
