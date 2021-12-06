081ce560a2faeecb719929ffd6157d672c25dd80
daniellemoreno525 <daniellemoreno525@tamu.edu>
2021-12-06 01:22:33 -0600

    changing name to level

--- server.js
+++ server.js
@@ -385,7 +385,7 @@ socket.on('getRank',username => {
       level = body.toString();
       level = level.substring(level.indexOf("level")+7, level.indexOf("toNextLevelPercent")-2);
       console.log(username+'\'s level: '+level);
-      socket.emit('retRank',level);
+      socket.emit('level',level);
     });
   });
 
